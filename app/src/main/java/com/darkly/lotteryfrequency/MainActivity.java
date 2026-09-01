790d95bc06fbbb0c5055fdb55667a7278f0c8641
ADarklyScanner <johnnyleelarue@gmail.com>
2026-09-01 09:26:04 +0000

    Fix manual ticket check bonus display

--- app/src/main/java/com/darkly/lotteryfrequency/MainActivity.java
+++ app/src/main/java/com/darkly/lotteryfrequency/MainActivity.java
@@ -1,7 +1,11 @@
 package com.darkly.lotteryfrequency;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -13,6 +17,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        if (window != null) {
+            window.setStatusBarColor(Color.argb(204, 28, 12, 43));
+            window.setNavigationBarColor(Color.argb(204, 28, 12, 43));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                window.getDecorView().setSystemUiVisibility(
+                    window.getDecorView().getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                );
+            }
+        }
+
         webView = new WebView(this);
         setContentView(webView);
 
